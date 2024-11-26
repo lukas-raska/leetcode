bfafd66b9e9f4652f0b614f2b8b7fe7b23fb46f6
lukas-raska <luky.raska@gmail.com>
2024-11-26 10:47:13 +0100

    task 0001 solution

---
+++ src/test/java/leetcode/task_0001/two_sum/Solution0001Test.java
@@ -0,0 +1,17 @@
+package leetcode.task_0001.two_sum;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution0001Test {
+
+    private Solution0001 solution0001 = new Solution0001();
+
+    @Test
+    void twoSumTest() {
+        assertArrayEquals(new int[] {0,1}, solution0001.twoSum(new int[]{2,7,11,15}, 9));
+        assertArrayEquals(new int[] {1,2}, solution0001.twoSum(new int[]{3,2,4}, 6));
+        assertArrayEquals(new int[] {0,1}, solution0001.twoSum(new int[]{3,3}, 6));
+    }
+}
