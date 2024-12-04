2bb25f47e11f0fc9fa63df40f244727d7ddb4a15
lukas-raska <luky.raska@gmail.com>
2024-12-04 11:56:42 +0100

    task 2109 solution

---
+++ src/main/java/leetcode/task_2109_addinng_spaces_to_a_string/Solution2109.java
@@ -0,0 +1,17 @@
+package leetcode.task_2109_addinng_spaces_to_a_string;
+
+public class Solution2109 {
+
+    public String addSpaces(String s,
+                            int[] spaces) {
+
+        StringBuilder result = new StringBuilder();
+        int from = 0;
+        for (int space: spaces){
+            result.append(s, from, space).append(" ");
+            from = space;
+        }
+        result.append(s,from,s.length());
+        return result.toString();
+    }
+}
