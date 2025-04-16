e5bdfe25e453e6b2453be2328d15fed9c671a7b7
lukas-raska <luky.raska@gmail.com>
2025-04-16 18:53:27 +0200

    ?

--- src/main/java/leetcode/task_0017_letter_combinations_of_a_phone_number/Solution0017.java
+++ src/main/java/leetcode/task_0017_letter_combinations_of_a_phone_number/Solution0017.java
@@ -1,2 +1,24 @@
-package leetcode.task_0017_letter_combinations_of_a_phone_number;public class Solution0017 {
+package leetcode.task_0017_letter_combinations_of_a_phone_number;
+
+import java.util.List;
+import java.util.Map;
+
+public class Solution0017 {
+
+    private static final Map<String, String[]> digitsMap = Map.of(
+            "0", new String[]{},
+            "1", new String[]{},
+            "2", new String[]{"a","b","c"},
+            "3", new String[ ]{"d","e","f"},
+            "3", new String[ ]{"d","e","f"},
+            "3", new String[ ]{"d","e","f"},
+            "3", new String[ ]{"d","e","f"},
+            "3", new String[ ]{"d","e","f"},
+            "3", new String[ ]{"d","e","f"},
+            "3", new String[ ]{"d","e","f"},
+    )
+
+    public List<String> letterCombinations(String digits){
+
+    }
 }
