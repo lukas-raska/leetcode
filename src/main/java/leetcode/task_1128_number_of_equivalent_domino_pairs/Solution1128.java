8e6c0a96fb15083c28e4464bc5cb62ca96721288
lukas-raska <luky.raska@gmail.com>
2025-05-06 20:21:16 +0200

    refactoring numEquivDominoPairs - using integer for mapping -> much faster

--- src/main/java/leetcode/task_1128_number_of_equivalent_domino_pairs/Solution1128.java
+++ src/main/java/leetcode/task_1128_number_of_equivalent_domino_pairs/Solution1128.java
@@ -1,33 +1,27 @@
 package leetcode.task_1128_number_of_equivalent_domino_pairs;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
+
 
 public class Solution1128 {
 
     public int numEquivDominoPairs(int[][] dominoes) {
+        Map<Integer, Integer> pairsCountMap = new HashMap<>();
 
-        Map<List<Integer>, Integer> pairsCountMap = new HashMap<>();
+        for (int[] domino : dominoes) {
+            int a = domino[0];
+            int b = domino[1];
+            int dominoValue = (a <= b) ? (10 * a + b) : (10 * b + a);
+            pairsCountMap.computeIfPresent(dominoValue, (k, v) -> v + 1);
+            pairsCountMap.putIfAbsent(dominoValue, 1);
 
-        for (int[] dominoe : dominoes) {
-
-            var current = List.of(dominoe[0], dominoe[1]);
-            var rotated = List.of(dominoe[1], dominoe[0]);
-
-            if (pairsCountMap.containsKey(current)) {
-                pairsCountMap.merge(current, 1, Integer::sum);
-            } else if (pairsCountMap.containsKey(rotated)) {
-                pairsCountMap.merge(rotated, 1, Integer::sum);
-            } else {
-                pairsCountMap.put(current, 1);
-            }
         }
-
         return pairsCountMap.values().stream()
                 .mapToInt(this::getPairCount)
                 .sum();
     }
+
 
     public int getPairCount(int elements) {
         int pairs = 0;
