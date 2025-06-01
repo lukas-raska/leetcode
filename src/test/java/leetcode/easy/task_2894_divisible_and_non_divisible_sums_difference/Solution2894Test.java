d5f04f16ea297fb80972a8bb24022c2b84ff0f4e
lukas-raska <luky.raska@gmail.com>
2025-06-01 09:26:00 +0200

    task 2894 solution

---
+++ src/test/java/leetcode/easy/task_2894_divisible_and_non_divisible_sums_difference/Solution2894Test.java
@@ -0,0 +1,16 @@
+package leetcode.easy.task_2894_divisible_and_non_divisible_sums_difference;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Solution2894Test {
+
+    Solution2894 solution2894 = new Solution2894();
+
+    @Test
+    public void example1(){
+        assertEquals(19, solution2894.differenceOfSums(10,3));
+    }
+
+}
