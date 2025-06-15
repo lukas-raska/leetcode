a328081268852e6ac9c0dfe4642eaf26a7cb7069
lukas-raska <luky.raska@gmail.com>
2025-06-15 20:11:22 +0200

    task 3079 solution

---
+++ src/main/java/leetcode/easy/_3079_find_the_sum_of_encrypted_integers/Solution3079.java
@@ -0,0 +1,34 @@
+package leetcode.easy._3079_find_the_sum_of_encrypted_integers;
+
+public class Solution3079 {
+
+    public int sumOfEncryptedInt(int[] nums) {
+        int sum = 0;
+        for (int n : nums) {
+            sum += encrypt(n);
+        }
+        return sum;
+    }
+
+    private int encrypt(int x) {
+        if (x < 10) {
+            return x;
+        }
+        int result = 0;
+        int digitsCounter = 0;
+        int maxDigit = 0;
+
+        while (x >= 1) {
+            int digit = x % 10;
+            if (digit > maxDigit) {
+                maxDigit = digit;
+            }
+            x /= 10;
+            digitsCounter++;
+        }
+        for (int e = 0; e < digitsCounter; e++) {
+            result += maxDigit * (int) Math.pow(10, e);
+        }
+        return result;
+    }
+}
