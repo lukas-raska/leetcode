83f58980a9b29c4131968b5933c933c66080d6b5
lukas-raska <luky.raska@gmail.com>
2025-07-05 14:18:29 +0200

    task 1394 solution

--- src/main/java/leetcode/easy/task_1394_find_lucky_integer_in_an_array/Solution1394.java
+++ src/main/java/leetcode/easy/task_1394_find_lucky_integer_in_an_array/Solution1394.java
@@ -1,4 +1,17 @@
 package leetcode.easy.task_1394_find_lucky_integer_in_an_array;
 
 public class Solution1394 {
+
+    public int findLucky(int[] arr) {
+        int[] frequencies = new int[501];
+        for (int i : arr) {
+            frequencies[i]++;
+        }
+        for (int i = frequencies.length - 1; i > 0; i--) {
+            if (i == frequencies[i]) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
