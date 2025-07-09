2149fcd83a8b4303dcef8841213e4ad21125c3ca
lukas-raska <luky.raska@gmail.com>
2025-07-09 18:46:42 +0200

    task 0136 solution

---
+++ src/main/java/leetcode/easy/task_0136_single_number/Solution0136.java
@@ -0,0 +1,12 @@
+package leetcode.easy.task_0136_single_number;
+
+public class Solution0136 {
+
+    public int singleNumber(int[] nums) {
+        int res = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            res ^= nums[i];
+        }
+        return res;
+    }
+}
