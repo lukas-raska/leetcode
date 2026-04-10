7d7ef70b16a35bab888e4fcd8c60a87ceec64412
lukas-raska <luky.raska@gmail.com>
2026-04-10 06:50:00 +0200

    task 3740 solution

---
+++ src/main/java/leetcode/easy/task_3740_minimum_distance_between_three_equal_elements_I/Solution3740.java
@@ -0,0 +1,45 @@
+package leetcode.easy.task_3740_minimum_distance_between_three_equal_elements_I;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Solution3740 {
+
+    public int minimumDistance(int[] nums) {
+
+        Map<Integer, List<Integer>> indexesMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            indexesMap.putIfAbsent(nums[i], new ArrayList<>(3));
+            indexesMap.get(nums[i]).add(i);
+        }
+
+        int minDistance = Integer.MAX_VALUE;
+        for (var indexes:indexesMap.values()){
+            if (indexes.size()>=3){
+                for (int i = 0; i < indexes.size()-2; i++) {
+                    int distance = distance(indexes.get(i), indexes.get(i+1), indexes.get(i+2));
+                    if (distance < minDistance){
+                        minDistance = distance;
+                    }
+                }
+            }
+        }
+
+        if (minDistance == Integer.MAX_VALUE){
+            minDistance = -1;
+        }
+
+        return minDistance;
+
+    }
+
+    private int distance(int i, int j, int k){
+        int ij = Math.abs(i-j);
+        int jk = Math.abs(j-k);
+        int ki = Math.abs(k-i);
+        return ij + jk + ki;
+    }
+}
