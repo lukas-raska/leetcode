c8d65f581b44a4df45c08b4a1db11074e606066a
lukas-raska <luky.raska@gmail.com>
2026-04-11 15:19:14 +0200

    task 3740 refactored

--- src/main/java/leetcode/easy/task_3740_minimum_distance_between_three_equal_elements_I/Solution3740.java
+++ src/main/java/leetcode/easy/task_3740_minimum_distance_between_three_equal_elements_I/Solution3740.java
@@ -9,6 +9,29 @@ public class Solution3740 {
 
     public int minimumDistance(int[] nums) {
 
+        Map<Integer, int[]> indexesMap = new HashMap<>();
+        int minDistance = Integer.MAX_VALUE;
+
+        for (int i = 0; i < nums.length; i++) {
+            indexesMap.putIfAbsent(nums[i], new int[3]);
+            var indexes = indexesMap.get(nums[i]);
+            int storedIndexes = indexes[0];
+            if (storedIndexes < 2) {
+                indexes[1 + indexes[0]++]= i;
+            } else {
+                int distance = distance(indexes[1], indexes [2], i);
+                if (distance < minDistance) {
+                    minDistance = distance;
+                }
+                indexes[1] = indexes[2];
+                indexes[2] = i;
+            }
+        }
+        return (minDistance == Integer.MAX_VALUE) ? -1 : minDistance;
+    }
+
+    public int minimumDistance2(int[] nums) {
+
         Map<Integer, List<Integer>> indexesMap = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
@@ -17,18 +40,18 @@ public class Solution3740 {
         }
 
         int minDistance = Integer.MAX_VALUE;
-        for (var indexes:indexesMap.values()){
-            if (indexes.size()>=3){
-                for (int i = 0; i < indexes.size()-2; i++) {
-                    int distance = distance(indexes.get(i), indexes.get(i+1), indexes.get(i+2));
-                    if (distance < minDistance){
+        for (var indexes : indexesMap.values()) {
+            if (indexes.size() >= 3) {
+                for (int i = 0; i < indexes.size() - 2; i++) {
+                    int distance = distance(indexes.get(i), indexes.get(i + 1), indexes.get(i + 2));
+                    if (distance < minDistance) {
                         minDistance = distance;
                     }
                 }
             }
         }
 
-        if (minDistance == Integer.MAX_VALUE){
+        if (minDistance == Integer.MAX_VALUE) {
             minDistance = -1;
         }
 
@@ -36,10 +59,12 @@ public class Solution3740 {
 
     }
 
-    private int distance(int i, int j, int k){
-        int ij = Math.abs(i-j);
-        int jk = Math.abs(j-k);
-        int ki = Math.abs(k-i);
+    private int distance(int i,
+                         int j,
+                         int k) {
+        int ij = Math.abs(i - j);
+        int jk = Math.abs(j - k);
+        int ki = Math.abs(k - i);
         return ij + jk + ki;
     }
 }
