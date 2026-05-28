afa43d72ae07fe3da655957583093dfe45d78e93
lukas-raska <luky.raska@gmail.com>
2026-05-28 12:20:25 +0200

    task 3093 refactor

--- src/main/java/leetcode/hard/task_3093_longest_common_suffix_queries/Solution3093.java
+++ src/main/java/leetcode/hard/task_3093_longest_common_suffix_queries/Solution3093.java
@@ -1,8 +1,10 @@
 package leetcode.hard.task_3093_longest_common_suffix_queries;
 
+import java.util.Arrays;
+
 class TrieNode {
 
-    private final static int MAX_WORD_LENGTH = 5_001;
+    private final static int MAX_WORD_LENGTH = Integer.MAX_VALUE;
     private final static int ALPHABET_LENGTH = 'z' - 'a' + 1;
     int optimalWordIndex = 0;
     int minWordLength = MAX_WORD_LENGTH;
@@ -20,23 +22,19 @@ public class Solution3093 {
 
         TrieNode root = createSuffixTrieNode(wordsContainer);
         int[] result = new int[wordsQuery.length];
+        Arrays.fill(result, root.optimalWordIndex);
 
         for (int i = 0; i < wordsQuery.length; i++) {
             String query = wordsQuery[i];
             TrieNode current = root;
-            boolean foundMatch = false;
             for (int j = query.length() - 1; j >= 0; j--) {
                 int idx = query.charAt(j) - 'a';
                 if (current.children[idx] != null) {
-                    result[i] = current.children[idx].optimalWordIndex;
-                    foundMatch = true;
+                    current = current.children[idx];
+                    result[i] = current.optimalWordIndex;
                 } else {
-                    if (!foundMatch) {
-                        result[i] = root.optimalWordIndex;
-                    }
                     break;
                 }
-                current = current.children[idx];
             }
         }
 
@@ -52,34 +50,30 @@ public class Solution3093 {
             TrieNode current = root;
             String word = wordsContainer[i];
 
+            updateNodeProperties(root, word.length(), i);
+
             for (int j = word.length() - 1; j >= 0; j--) {
                 char currentChar = word.charAt(j);
                 int idx = currentChar - 'a';
-
                 if (current.children[idx] == null) {
                     current.children[idx] = new TrieNode();
                 }
-
-                if (word.length() < root.minWordLength) {
-                    root.optimalWordIndex = i;
-                    root.minWordLength = word.length();
-                }
-
-                if (word.length() < current.children[idx].minWordLength) {
-                    current.children[idx].optimalWordIndex = i;
-                    current.children[idx].minWordLength = word.length();
-
-
-                } else if (word.length() == current.children[idx].minWordLength) {
-                    current.children[idx].optimalWordIndex = Math.min(i, current.children[idx].optimalWordIndex);
-                    root.optimalWordIndex = Math.min(i, root.optimalWordIndex);
-                }
-
+                updateNodeProperties(current.children[idx], word.length(), i);
                 current = current.children[idx];
             }
-
         }
         return root;
+    }
+
+    private void updateNodeProperties(TrieNode node,
+                                      int wordLength,
+                                      int index) {
+        if (node.minWordLength > wordLength) {
+            node.optimalWordIndex = index;
+            node.minWordLength = wordLength;
+        } else if (node.minWordLength == wordLength) {
+            node.optimalWordIndex = Math.min(node.optimalWordIndex, index);
+        }
     }
 
 }
