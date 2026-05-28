df322c816b5816fccdd120c99421eff183199b2e
lukas-raska <luky.raska@gmail.com>
2026-05-28 11:57:30 +0200

    task 3093 solution

--- src/main/java/leetcode/hard/task_3093_longest_common_suffix_queries/Solution3093.java
+++ src/main/java/leetcode/hard/task_3093_longest_common_suffix_queries/Solution3093.java
@@ -1,65 +1,85 @@
 package leetcode.hard.task_3093_longest_common_suffix_queries;
 
-public class Solution3093 {
+class TrieNode {
 
-    static class TrieNode {
-        TrieNode[] children;
+    private final static int MAX_WORD_LENGTH = 5_001;
+    private final static int ALPHABET_LENGTH = 'z' - 'a' + 1;
+    int optimalWordIndex = 0;
+    int minWordLength = MAX_WORD_LENGTH;
+    TrieNode[] children;
 
-        TrieNode() {
-            children = new TrieNode['z' - 'a' + 1];
-        }
+    TrieNode() {
+        children = new TrieNode[ALPHABET_LENGTH];
     }
+}
+
+public class Solution3093 {
 
     public int[] stringIndices(String[] wordsContainer,
                                String[] wordsQuery) {
 
-        TrieNode root = new TrieNode();
-        for(String word: wordsContainer){
-            insertReversedWord(root,word);
-        }
+        TrieNode root = createSuffixTrieNode(wordsContainer);
+        int[] result = new int[wordsQuery.length];
 
-        int [] result = new int[wordsQuery.length];
-        for (int i = 0; i < wordsQuery.length; i++){
-            String commonSuffix = getCommonSuffix(root,wordsQuery[i]);
-            int minLength = Integer.MAX_VALUE;
-            for (int j = 0; j < wordsContainer.length; j++) {
-                if (wordsContainer[j].endsWith(commonSuffix) && wordsContainer[j].length() < minLength){
-                    minLength = wordsContainer[j].length();
-                    result[i]= j;
+        for (int i = 0; i < wordsQuery.length; i++) {
+            String query = wordsQuery[i];
+            TrieNode current = root;
+            boolean foundMatch = false;
+            for (int j = query.length() - 1; j >= 0; j--) {
+                int idx = query.charAt(j) - 'a';
+                if (current.children[idx] != null) {
+                    result[i] = current.children[idx].optimalWordIndex;
+                    foundMatch = true;
+                } else {
+                    if (!foundMatch) {
+                        result[i] = root.optimalWordIndex;
+                    }
+                    break;
                 }
+                current = current.children[idx];
             }
         }
 
         return result;
-
     }
 
-    private void insertReversedWord(TrieNode root,
-                                    String word) {
-        TrieNode current = root;
-        for (int i = word.length() - 1; i >= 0; i--) {
-            int idx = word.charAt(i)- 'a';
-            if (current.children[idx] == null){
-                current.children[idx] = new TrieNode();
-            }
-            current = current.children[idx];
-        }
-    }
+    private TrieNode createSuffixTrieNode(String[] wordsContainer) {
 
-    private String getCommonSuffix(TrieNode root, String query){
+        TrieNode root = new TrieNode();
 
-        StringBuilder commonSuffix = new StringBuilder();
-        TrieNode current = root;
+        for (int i = 0; i < wordsContainer.length; i++) {
 
-        for (int i = query.length()-1; i >=0 ; i--) {
-            int idx = query.charAt(i) - 'a';
-            if (current.children[idx]!= null){
-                commonSuffix.append(query.charAt(i));
+            TrieNode current = root;
+            String word = wordsContainer[i];
+
+            for (int j = word.length() - 1; j >= 0; j--) {
+                char currentChar = word.charAt(j);
+                int idx = currentChar - 'a';
+
+                if (current.children[idx] == null) {
+                    current.children[idx] = new TrieNode();
+                }
+
+                if (word.length() < root.minWordLength) {
+                    root.optimalWordIndex = i;
+                    root.minWordLength = word.length();
+                }
+
+                if (word.length() < current.children[idx].minWordLength) {
+                    current.children[idx].optimalWordIndex = i;
+                    current.children[idx].minWordLength = word.length();
+
+
+                } else if (word.length() == current.children[idx].minWordLength) {
+                    current.children[idx].optimalWordIndex = Math.min(i, current.children[idx].optimalWordIndex);
+                    root.optimalWordIndex = Math.min(i, root.optimalWordIndex);
+                }
+
                 current = current.children[idx];
-            } else {
-                break;
             }
+
         }
-        return commonSuffix.reverse().toString();
+        return root;
     }
+
 }
