e142f7a24cb35a59aa516a05fb9841c122038c75
lukas-raska <luky.raska@gmail.com>
2026-05-31 18:33:55 +0200

    task 2126 solution without sorting

--- src/main/java/leetcode/medium/task_2126_destroying_asteroids/Solution2126.java
+++ src/main/java/leetcode/medium/task_2126_destroying_asteroids/Solution2126.java
@@ -4,31 +4,61 @@ import java.util.*;
 
 public class Solution2126 {
 
+
     public boolean asteroidsDestroyed(int mass,
                                       int[] asteroids) {
 
-        Arrays.sort(asteroids);
+        final int MAX_ASTEROID_MASS = 500_000;
+        int size = asteroids.length;
+        int[] rest = new int[size];
+        int restSize = 0;
 
-        long currentMass = mass;
-        long massGained = 0;
-        int lastDestroyed = -1;
-        for (int i = 0; i < asteroids.length; i++) {
+        boolean found;
+        do {
+            found = false;
+            for (int i = 0; i < size; i++) {
 
-            if (asteroids[i] <= currentMass) {
-                massGained += asteroids[i];
-                lastDestroyed = i;
-            } else {
-                currentMass += massGained;
-                massGained = 0;
-                if (asteroids[i] <= currentMass) {
-                    lastDestroyed = i;
-                    currentMass += asteroids[i];
+                if (mass >= MAX_ASTEROID_MASS) {
+                    return true;
+                }
+
+                if (asteroids[i] <= mass) {
+                    mass += asteroids[i];
+                    found = true;
                 } else {
-                    break;
+                    rest[restSize++] = asteroids[i];
                 }
             }
+            if (restSize == 0) {
+                return true;
+            } else {
+                asteroids = rest;
+                size = restSize;
+                restSize = 0;
+            }
         }
+        while (found);
 
-        return lastDestroyed == asteroids.length - 1;
+        return false;
     }
+
+    public boolean asteroidsDestroyed2(int mass,
+                                       int[] asteroids) {
+
+        final int MAX_ASTEROID_MASS = 500_000;
+        Arrays.sort(asteroids);
+        for (int a : asteroids) {
+            if (a <= mass) {
+                mass += a;
+            } else {
+                return false;
+            }
+            if (mass > MAX_ASTEROID_MASS) {
+                break;
+            }
+        }
+        return true;
+    }
+
+
 }
